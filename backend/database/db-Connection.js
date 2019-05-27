8a672ba3182c5873107c2a45ad0abdd6baeb5d65
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-05-26 23:15:45 -0300

    Database connection configuration

---
+++ backend/database/db-Connection.js
@@ -0,0 +1,19 @@
+const mongo = require('mongodb');
+
+var connMongoDB = function(){
+	var db = new mongo.Db(
+		'975system',
+		new mongo.Server(
+			'localhost', //string contendo o endereço do servidor
+			27017, //porta de conexão
+			{}
+		),
+		{}
+	);
+
+	return db;
+}
+
+module.exports = function(){
+	return connMongoDB;
+}
