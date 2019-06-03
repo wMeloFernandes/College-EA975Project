c4e2c88404f44301df055e3655c3bb0245c03e25
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-06-02 23:11:22 -0300

    Create user method

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -7,7 +7,25 @@ module.exports.getUserByID = (app, req, res)=>{
 }
 
 module.exports.createUser = (app, req, res)=>{
+    const userModel = {
+        name: req.body.name,
+        job: req.body.job,
+        email: req.body.email,
+        password: req.body.password
+    };
     
+    const AdminDAO = new app.database.dao.AdminDAO();
+    AdminDAO.createUser(userModel, (err)=>{
+        if(err){
+            console.error(err);
+            res.send('Problem when create new user');
+        }else{
+            res.status(200).json({
+                message: "User created!"
+            });
+        }
+       
+    });   
 }
 
 module.exports.updateUSer = (app, req, res)=>{
--- backend/database/dao/AdminDAO.js
+++ backend/database/dao/AdminDAO.js
@@ -1,7 +1,17 @@
-function AdminDAO(connection){
-	this._connection = connection();
+const userModel = require('./../../models/user-model');
+function AdminDAO(){
 }
 
+AdminDAO.prototype.createUser = function(data, callback){
+	var db = new userModel();
+    db.name = data.name;
+    db.job = data.job;
+    db.email = data.email;
+    db.password  = data.password;
+    db.save((err)=>{
+        callback(err);
+    });  
+}
 
 
 module.exports = function(){
