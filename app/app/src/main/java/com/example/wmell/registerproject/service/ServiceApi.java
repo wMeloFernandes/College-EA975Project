f17bc7a9098dc58fa904ccb89f1ba9b174e07a4c
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-06-09 23:09:10 -0300

    Adding other api calls

--- app/app/src/main/java/com/example/wmell/registerproject/service/ServiceApi.java
+++ app/app/src/main/java/com/example/wmell/registerproject/service/ServiceApi.java
@@ -1,12 +1,15 @@
 package com.example.wmell.registerproject.service;
 
 import com.example.wmell.registerproject.model.LoginResponse;
+import com.example.wmell.registerproject.model.RegisterResponse;
+import com.example.wmell.registerproject.model.Response;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
-
+import retrofit2.http.Query;
 
 
 public interface ServiceApi {
@@ -14,4 +17,12 @@ public interface ServiceApi {
     @FormUrlEncoded
     Call<LoginResponse> login(@Field("email") String username,
                               @Field("password") String password);
+
+    @POST("/user")
+    @FormUrlEncoded
+    Call<Response> clockIn(@Field("email") String email,
+                           @Field("timestamp") String timestamp);
+
+    @GET("/user")
+    Call<RegisterResponse> getRegister(@Query("email") String email);
 }
