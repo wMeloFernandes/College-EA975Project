987737a2d70513b8f7575b89ed3a291db6ccc07d
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-06-09 23:38:40 -0300

    Reading QR Code

--- app/app/src/main/java/com/example/wmell/registerproject/MainActivity.java
+++ app/app/src/main/java/com/example/wmell/registerproject/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.wmell.registerproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mRegisterButton;
     private ImageView mCameraButton;
+    public static int REQUEST_CAMERA_CODE = 100;
+    public static String QRCodeResult = "QRCodeResult";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +39,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Intent i = new Intent(MainActivity.this, QRCodeActivity.class);
-            startActivity(i);
+            startActivityForResult(i, REQUEST_CAMERA_CODE);
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CAMERA_CODE){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, data.getStringExtra(QRCodeResult), Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Problem to read QR Code", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     public void onBackPressed() {
--- app/app/src/main/java/com/example/wmell/registerproject/QRCodeActivity.java
+++ app/app/src/main/java/com/example/wmell/registerproject/QRCodeActivity.java
@@ -1,9 +1,13 @@
 package com.example.wmell.registerproject;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
 
 import java.util.ArrayList;
 
@@ -12,12 +16,13 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class QRCodeActivity extends AppCompatActivity {
 
     private ZXingScannerView scannerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
 
-        ArrayList<BarcodeFormat> list =  new ArrayList<>();
+        ArrayList<BarcodeFormat> list = new ArrayList<>();
         list.add(BarcodeFormat.QR_CODE);
 
         scannerView = findViewById(R.id.qrCode_id);
@@ -31,7 +36,7 @@ public class QRCodeActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         scannerView.startCamera();
-        scannerView.setResultHandler((ZXingScannerView.ResultHandler) this);
+        scannerView.setResultHandler(resultHandler);
     }
 
     @Override
@@ -39,4 +44,15 @@ public class QRCodeActivity extends AppCompatActivity {
         super.onPause();
         scannerView.stopCamera();
     }
+
+    ZXingScannerView.ResultHandler resultHandler = new ZXingScannerView.ResultHandler() {
+        @Override
+        public void handleResult(Result result) {
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra(MainActivity.QRCodeResult, result.getText());
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        }
+    };
+
 }
