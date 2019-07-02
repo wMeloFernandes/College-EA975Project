7520a4eedc8f115572849b29cc46acc6b254500d
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-07-01 23:40:37 -0300

    Adding necessary headers

--- backend/config/server.js
+++ backend/config/server.js
@@ -6,6 +6,19 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+	  "Access-Control-Allow-Headers",
+	  "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.setHeader(
+	  "Access-Control-Allow-Methods",
+	  "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+	);
+	next();
+  });
+
 consign()
 	.include('/routes')
 	.then('/controllers')
