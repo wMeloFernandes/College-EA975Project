ccf8d9aa05ed734818dd746c6b6aac58d56f65f7
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-07-02 00:44:45 -0300

    Implementing new user

--- frontend/frontend/src/app/new-user/new-user.component.ts
+++ frontend/frontend/src/app/new-user/new-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from './../app.service';
 
 @Component({
   selector: 'app-new-user',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  private name: string;
+  private job: string;
+  private email: string;
+  private password: string;
+
+
+  constructor(public appService: AppService) { }
 
   ngOnInit() {
+  }
+
+  onGetName(event: any){
+    this.name = event.target.value;
+  }
+
+  onGetJob(event: any){
+    this.job = event.target.value;
+  }
+
+  onGetEmail(event: any){
+    this.email = event.target.value;
+  }
+
+  onGetPassword(event: any){
+    this.password = event.target.value;
+  }
+
+  createUser(){
+    this.appService.createUser(this.name, this.job, this.email, this.password);
   }
 
 }
