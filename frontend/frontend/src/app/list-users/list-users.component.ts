59b23e232f511f9c273442639d95a296174c6354
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-07-02 01:02:34 -0300

    Removing unnecessary lines

--- frontend/frontend/src/app/list-users/list-users.component.ts
+++ frontend/frontend/src/app/list-users/list-users.component.ts
@@ -2,16 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppService } from './../app.service';
 
 import {User} from '../user';
-
-const USERS: User[] = [
-    {id:1, name: "Zé Roberto Magalhães" , email: "zrmaga@gmail.com", pontos: [new Date(), new Date()]},
-    {id:2, name: "Humberto Magalhães" , email: "zrmaga@gmail.com", pontos: [new Date(), new Date()]},
-    {id:3, name: "Doisberto Magalhães" , email: "zrmaga@gmail.com", pontos: [new Date(), new Date()]},
-    {id:4, name: "Trêsberto Magalhães" , email: "zrmaga@gmail.com", pontos: [new Date(), new Date()]},
-    {id:5, name: "Quatroberto Magalhães" , email: "zrmaga@gmail.com", pontos: [new Date(), new Date()]},
-]
     
-
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
@@ -20,7 +11,6 @@ const USERS: User[] = [
 export class ListUsersComponent implements OnInit {
   selectedUser: User;
   userList: any;
-  users: any;
 
   constructor(public appService: AppService) { }
 
@@ -28,8 +18,6 @@ export class ListUsersComponent implements OnInit {
     this.appService.getAllUsers()
       .subscribe(resp=>{
         this.userList = resp['users'];
-        console.log(this.userList);
-        this.users = this.userList;
       });
   }
 
