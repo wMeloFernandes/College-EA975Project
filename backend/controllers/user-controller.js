1acac0427695f2c0360aee74526bbe8a2a3f5358
WIllian Melo <w.melo.fernandes.un@gmail.com>
2019-07-01 23:50:18 -0300

    Improving backend answer

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -8,7 +8,9 @@ module.exports.clockIn = (app, req, res)=>{
     ClockDAO.clockIn(clockModel, (err)=>{
         if(err){
             console.error(err);
-            res.send('Problem when register time');
+            res.status(500).json({
+                message: 'Problem when register time'
+            });
         }else{
             res.status(200).json({
                 message: "Time registered!"
